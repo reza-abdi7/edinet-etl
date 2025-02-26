46b64d57978f1a58a166fdc579c7c90f91546181
Reza Abdi <reza.abdi@live.com>
2025-02-26 18:30:55 +0100

    add load script

--- src/load.py
+++ src/load.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from .utils.logger import logger
 
-def load_to_csv(df: pd.DataFrame, output_path: str) -> None:
+def load_to_csv(df: pd.DataFrame, output_path: str, file_name: str) -> None:
     """
     Writes the given DataFrame to a CSV file at the specified path.
 
@@ -14,12 +14,11 @@ def load_to_csv(df: pd.DataFrame, output_path: str) -> None:
         logger.warning("Attempted to load an empty DataFrame to CSV. Aborting.")
         return
     
-    
     try:
-        # Create output directory if it doesn't exist
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        full_file_path = os.path.join(output_path, file_name)
+        os.makedirs(os.path.dirname(full_file_path), exist_ok=True)
         
-        df.to_csv(output_path, index=False, encoding='utf-8')
+        df.to_csv(full_file_path, index=False, encoding='utf-8')
         logger.info(f"Data successfully loaded to CSV: {output_path}")
     except Exception as e:
         logger.error(f"Error while writing CSV to {output_path}: {e}")
